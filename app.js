14d44af4e3ba56489c386a5fb7b58b9882701cdb
Bernardo Robaina <bernardorobaina@MacBook-de-Bernardo.local>
2021-01-20 14:31:38 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require(`express`);
+const bodyparser = require(`body-parser`);
+
+const app = express();
+
+app.use(bodyparser.json());
+
+app.use((req, res, next) => {
+    res.setHeader(`Access-Control-Allow-Origin`, `*`);
+    res.setHeader(`Access-Control-Allow-Methods`, `OPTIONS, GET, POST, PUT, PATCH, DELETE`);
+    res.setHeader(`Access-Control-Allow-Headers`, `Content-Type, Authorization`);
+    next();
+});
+
+
+
+app.listen(8080);
